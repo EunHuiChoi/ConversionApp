25f20f48f2df4d549d95e2512ed387d329e3621b
EunHuiChoi <eun95828@likelion.org>
2019-10-17 02:21:57 +1100

    Third

--- app/src/main/java/com/example/conversionapp/Conversion.java
+++ app/src/main/java/com/example/conversionapp/Conversion.java
@@ -4,13 +4,28 @@ public class Conversion {
 
     public String ResultToCenti(String sInch, String sFoot, String sMile) {
         try {
-            double inch = Double.parseDouble(sInch);
-            double foot = Double.parseDouble(sFoot);
-            double mile = Double.parseDouble(sMile);
-
+            double inch;
+            double foot;
+            double mile;
+            if(sInch.isEmpty()){
+                inch = 0.0;
+            }else{
+                inch = Double.parseDouble(sInch);
+            }
+            if(sFoot.isEmpty()){
+                foot = 0.0;
+            }else{
+                foot = Double.parseDouble(sFoot);
+            }
+            if(sMile.isEmpty()){
+                mile = 0.0;
+            }else{
+                mile = Double.parseDouble(sMile);
+            }
             double centi = (inch * 2.54) + (foot * 30.48) + (mile * 160934.4);
 
             return String.format("%3.2f", centi);
+
         } catch (NumberFormatException nfe) {
             return "ERR";
         }
